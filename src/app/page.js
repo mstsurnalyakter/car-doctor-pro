a10a9fa0cc4b784cd926a9175dfe9d4e169ce705
mstsurnalyakter <surnalyms@gmail.com>
2024-06-22 19:06:50 +0600

    navbar added

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
+import Homepage from "@/components/Homepage/Homepage";
 
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello Car Doctor.
-      <button className="btn btn-primary">Button</button>
+    <main className="">
+      <Homepage/>
     </main>
   );
 }
