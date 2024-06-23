851f6fc66acaa0079e2cc1ddb4907c48e0407461
mstsurnalyakter <surnalyms@gmail.com>
2024-06-23 09:04:08 +0600

    mongodb setup

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/shared/NavBar";
 import Footer from "@/components/shared/Footer";
+import AuthProvider from "@/Services/AuthProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,13 +14,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" data-theme="carDoctorTheme">
-      <body className={inter.className}>
-        <NavBar />
-        <div className="min-h-[calc(100vh-345px)] mt-10 mb-20">
-        {children}
-        </div>
-        <Footer />
-      </body>
+        <body className={inter.className}>
+      <AuthProvider>
+          <NavBar />
+          <div className="min-h-[calc(100vh-345px)] mt-10 mb-20">
+            {children}
+          </div>
+          <Footer />
+      </AuthProvider>
+        </body>
     </html>
   );
 }
